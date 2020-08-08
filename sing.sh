a162d5d4ec233ecd34315d8dcdedead2d45e5113
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2020-08-09 04:20:37 +0500

    Add files via upload
    Singing Eminem's Rap God 
    backtrack : https://backingtracks.co/minusovka/43193/eminem/rap_god.html

---
+++ sing.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+say -v Victoria "Look, I was gonna go easy on you and not to hurt your feelings But I'm only" 
+say -v Victoria "going to get this one chance." -r 250
+say -v Victoria "Something's wrong, I can feel it" -r 150
+say "Just a feeling I've got, like something's about to happen, but I don't know what If that means, what I think it means, we're in trouble, big trouble, And if he is as bananas as you say, I'm not taking any chances" -r 400 
+say "You were just what the doctor ordered" -r 300
+say "I'm beginning to feel like a Rap God, Rap God" -r 300
+say "All my people from the front to the back nod, back nod
+Now who thinks their arms are long enough to slap box, slap box?
+They said I rap like a robot, so call me Rapbot" -r 300
+say "But for me to rap like a computer must be in my genes" -r 400
+say "I got a laptop in my back pocket
+My pen'll go off when I half-cock it
+Got a fat knot from that rap profit
+Made a living and a killing off it
+Ever since Bill Clinton was still in office
+With Monica Lewinsky feeling on his nut sack
+I'm an MC still as honest
+But as rude and indecent as all hell syllables, killaholic (Kill 'em all with)
+This slickety, gibbedy, hibbedy hip hop
+You don't really wanna get into a pissing match with this rappidy brat
+Packing a Mac in the back of the Ac, backpack rap crap, yep, yackidy yac
+And at the exact same time I attempt these lyrical acrobat stunts while I'm practicing
+That I'll still be able to break a motherfuckin' table
+Over the back of a couple of faggots and crack it in half
+Only realized it was ironic I was signed to Aftermath after the fact
+How could I not blow? All I do is drop F-bombs, feel my wrath of attack
+Rappers are having a rough time period, here's a Maxipad
+It's actually disastrously bad
+For the wack while I'm masterfully constructing this masterpiece as" -r 550
+say "I'm beginning to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+Now who thinks their arms are long enough to slap box, slap box?
+Let me show you maintaining this shit ain't that hard, that hard" -r 360
+say "Everybody want the key and the secret to rap immortality like I have got
+Well, to be truthful the blueprint's simply rage and youthful exuberance
+Everybody loves to root for a nuisance" -r 360
+
+say "Hit the earth like an asteroid, did nothing but shoot for the moon since
+MC's get taken to school with this music
+'Cause I use it as a vehicle to bus the rhyme
+Now I lead a new school full of students
+Me? I'm a product of Rakim, Lakim Shabazz, 2Pac N-
+-W.A, Cube, hey, Doc, Ren, Yella, Eazy, thank you, they got Slim
+Inspired enough to one day grow up, blow up and be in a position
+To meet Run DMC and induct them into the motherfuckin' Rock n'
+Roll Hall of Fame" -r 550
+say "Even though I walk in the church and burst in a ball of flames
+Only Hall of Fame I be inducted in is the alcohol of fame
+On the wall of shame
+You fags think it's all a game 'til I walk a flock of flames
+Off of planking, tell me what in the fuck are you thinking?
+Little gay looking boy
+So gay I can barely say it with a straight face looking boy
+You witnessing a mass occur
+Like you watching a church gathering take place looking boy
+Oy vey, that boy's gay, that's all they say looking boy
+You get a thumbs up, pat on the back
+And a "way to go" from your label everyday looking boy" -r 360
+say "Hey, looking boy, what you say looking boy?
+I got a "hell yeah" from Dre looking boy
+I'mma work for everything I have
+Never ask nobody for shit, get outta my face looking boy
+Basically boy you're never gonna be capable
+To keep up with the same pace looking boy" -r 400
+say "'Cause I'm beginning to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+The way I'm racing around the track, call me Nascar, Nascar
+Dale Earnhardt of the trailer park, the White Trash God
+Kneel before General Zod this planet's Krypton, no Asgard, Asgard" -r 400
+say "Thanks for watching"
